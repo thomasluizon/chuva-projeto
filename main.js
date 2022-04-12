efd68e3fa32a82259c506f61795994fcf7eda4b0
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-04-12 15:23:05 -0300

    added comment feature

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const show = document.querySelector('#show');
+const comment = document.querySelectorAll('.item__main');
+
+// Ver mais
 show.addEventListener('click', () => {
    show.style.display = 'none';
    document.querySelector('#hidden').style.display = 'inline';
+});
+
+// Mostrar comentários dos tópicos
+comment.forEach(elem => {
+   elem.addEventListener('click', () => {
+      elem.nextElementSibling.classList.toggle('display');
+   });
 });
