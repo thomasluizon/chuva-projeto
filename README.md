7b34405a2dd47e5bc9a33f98a5de0637a8b3ee6d
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-04-12 22:20:51 -0300

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ## projeto-chuva
+
+Projeto feito por Thomas Luizon com HTML, CSS e JavaScript para processo seletivo.
+
+![Desktop-1](./assets/screenshots/desktop-1.png)
+![Desktop-2](./assets/screenshots/desktop-2.png)
+![Desktop-3](./assets/screenshots/desktop-3.png)
+![Desktop-4](./assets/screenshots/desktop-4.png)
+
+![Mobile-1](./assets/screenshots/mobile-1.png)
+![Mobile-2](./assets/screenshots/mobile-2.png)
+![Mobile-3](./assets/screenshots/mobile-3.png)
+![Mobile-4](./assets/screenshots/mobile-4.png)
+![Mobile-5](./assets/screenshots/mobile-5.png)
--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const createNewTopicBtn = document.querySelector('#create-new-topic');
 const state1 = document.querySelector('.state1');
 const state2 = document.querySelector('.state2');
 const state3 = document.querySelector('.state3');
+const formInput = document.querySelector('#topic');
+const textInput = document.querySelector('#content');
+const openDrawerBtn = document.querySelector('.burguer');
+const closeDrawerBtn = document.querySelector('.close');
+const drawer = document.querySelector('.sidebar');
+const sidebarItem = document.querySelectorAll('.sidebar__item');
 
 // Ver mais
 summary.addEventListener('click', () => {
@@ -39,9 +45,65 @@ topicForm.addEventListener('submit', e => {
    e.preventDefault();
    state2.classList.add('display');
    state3.classList.remove('display');
+   createItem(formInput.value, textInput.value);
+   formInput.value = '';
+   textInput.value = '';
 });
 
 createNewTopicBtn.addEventListener('click', () => {
    state3.classList.add('display');
    state2.classList.remove('display');
+});
+
+// Adicionar comentário
+
+function createItem(topic, text) {
+   const item = document.createElement('li');
+   item.classList.add('topics__item');
+   item.classList.add('over');
+   item.innerHTML = `
+   <div class="item__main blur">
+      <h3>${topic}</h3>
+      <h4>Thomas Luizon</h4>
+      <p class="item__comment">${text}</p>
+      <div class="topic__socials">
+         <button>
+            <img
+               src="assets/imgs/more-info.png"
+               alt=""
+            />
+         </button>
+         <button class="topic__socials-heart">
+            <img src="assets/imgs/heart.png" alt="" />
+         </button>
+         <p>0 likes</p>
+         <p>0 respostas</p>
+      </div>
+   </div>
+   <div class="overlay">
+      <img src="assets/imgs/blur-logo.png" alt="" />
+      <p>Aguardando feedback dos autores</p>
+      <a href="#">Editar tópico</a>
+   </div>
+   `;
+   document.querySelector('.topics__items').appendChild(item);
+}
+
+// Ativar drawer
+
+openDrawerBtn.addEventListener('click', () => {
+   drawer.classList.add('show');
+   document.body.classList.add('scroll');
+});
+
+closeDrawerBtn.addEventListener('click', () => {
+   drawer.classList.remove('show');
+   document.body.classList.remove('scroll');
+});
+
+sidebarItem.forEach(e => {
+   e.addEventListener('click', () => {
+      drawer.classList.remove('show');
+      document.body.classList.remove('scroll');
+   });
 });
